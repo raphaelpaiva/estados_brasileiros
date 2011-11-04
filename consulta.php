b46c32d9355504ae4265d82a3132d1d8d7e918a5
Raphael Paiva <raphaeldpaiva@gmail.com>
2011-11-04 10:37:24 -0200

    conectando no dbpedia

--- consulta.php
+++ consulta.php
@@ -6,6 +6,16 @@
 
 	<body>
 		<h1>Estados Brasileiros</h1>
+		<?php
+			require_once( "sparqllib.php" );
+			$db = sparql_connect( "http://dbpedia.org/sparql/" );
+
+			if ( !$db )
+			{
+				print "<p class=\"error\">" . sparql_errno() . ": " . sparql_error() . "</p>";
+				exit;
+			}
+		?>
 	</body>
 
 </html>
