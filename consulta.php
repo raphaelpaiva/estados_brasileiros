309a3ff7eb7b3184e3b0b101987d756fcabfd5f3
Raphael Paiva <raphaeldpaiva@gmail.com>
2011-11-04 10:25:09 -0200

    criando template para a consulta

---
+++ consulta.php
@@ -0,0 +1,11 @@
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<title>Estados Brasileiros</title>
+	</head>
+
+	<body>
+		<h1>Estados Brasileiros</h1>
+	</body>
+
+</html>
