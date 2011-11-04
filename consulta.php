95dfb17f1a56075fb146eb811b7c9d7fe47a3afd
raphaelpaiva <raphaeldpaiva@gmail.com>
2011-11-04 11:01:32 -0200

    inserindo algum estilo...

--- consulta.php
+++ consulta.php
@@ -1,6 +1,7 @@
 <html>
 	<head>
-		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <link rel="stylesheet" type="text/css"  href="css/style.css" />
 		<title>Estados Brasileiros</title>
 	</head>
 
